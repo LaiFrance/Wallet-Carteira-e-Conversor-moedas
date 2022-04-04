2ab4b8232311c3436b158dbf121ce75e9a657865
laise <laise94@hotmail.com>
2022-04-04 15:50:10 -0300

    instalando o thunk e add a store e add o applymiddleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,11 @@
 // estado global
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension'; // função
+import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
-const store = createStore(rootReducer, composeWithDevTools());
+const store = createStore(rootReducer, composeWithDevTools(
+  applyMiddleware(thunk),
+));
 
 export default store;
